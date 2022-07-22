ce28eff4179d4c1405649e8c0d7e8a72171a8abf
Kevin O <Kevinnuricumbo@gmail.com>
2022-07-22 11:49:02 -0500

    Vista agregada

--- main.py
+++ main.py
@@ -1,21 +1,66 @@
+from copy import copy
 from datetime import datetime
+from msilib.schema import Dialog
 from matplotlib.cbook import flatten
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 from pyparsing import java_style_comment
 import tensorflow as tf
-#import tensorflow_datasets as tfds una musca herramienta misteriosa
 import sys
 import os
+import cv2 as cv
 from tensorflow import keras
 from keras import layers
 from keras.models import Sequential
 import seaborn as sns
 import serial, time
+import shutil
+import threading
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog, QFileDialog
 
-def main():
+class Vista(QMainWindow):
+    nombre = ""
+    def __init__(self):
+        super().__init__()
+        uic.loadUi ("vista_identificador.ui", self)
+        self.btn_imagen.clicked.connect(self.obtener_imagen)
+        self.btn_nombre.clicked.connect(self.crear_carpeta)
+        self.btn_entrenar.clicked.connect(self.entrenar)
+        self.btn_iniciar.clicked.connect(self.iniciar)
+        
+    def crear_carpeta(self):
+        if len(self.nombre_persona.text()) > 0:
+            self.nombre = self.nombre_persona.text()
+            os.makedirs(f"dataset/test/{self.nombre}", exist_ok=True)
+            os.makedirs(f"dataset/Train/{self.nombre}", exist_ok=True)
+            os.makedirs(f"dataset/Validacion/{self.nombre}", exist_ok=True)
+            self.btn_imagen.setEnabled(True)
+            self.btn_nombre.setEnabled(False)
+            print("Carpeta creada")
+        else:
+            print("Campo vacio")
+        
+    def obtener_imagen(self):
+        imagen = QFileDialog.getSaveFileName(None, "Cargar imagen", "", "PNG(*.png);;JPEG(*.jpeg);;JPG(*.jpg);;All Files(*.*)")
+        src = cv.imread(imagen[0], cv.COLOR_BAYER_BG2BGR_VNG)
+        cv.imshow("Imagen", src)
+        shutil.copy(imagen[0], f"dataset/test/{self.nombre}")
+        shutil.copy(imagen[0], f"dataset/Train/{self.nombre}")
+        shutil.copy(imagen[0], f"dataset/Validacion/{self.nombre}")
+        print(imagen)
+        print("\n", imagen[0])
+        
+    def entrenar(self):
+        self.btn_imagen.setEnabled(False)
+        self.btn_nombre.setEnabled(True)
+        main(2)
+    
+    def iniciar(args):
+        main(1)
 
+def main(bandera):
     batch_size = 150
     img_height = 100
     img_width = 100
@@ -33,49 +78,17 @@ def main():
     train_ds = train_ds.cache().prefetch(buffer_size=10)
     val_ds = val_ds.cache().prefetch(buffer_size=10)
     num_clases = len(class_names)
-    # data_augmentation = tf.keras.Sequential(
-    # [
-    #     tf.keras.layers.RandomFlip("horizontal",
-    #                     input_shape=(img_height,
-    #                                 img_width,
-    #                                 3)),
-    #     tf.keras.layers.RandomRotation(0.1),
-    #     tf.keras.layers.RandomZoom(0.1),
-    # ]
-    # )
+
+    if bandera == 1:
+        print("Bandera: ", bandera)
+        if os.path.exists("new/prueba.h5") == False:
+            pass
+            modelo = crear_red(img_height, img_width, num_clases, train_ds, val_ds)
+        else:
+            modelo = tf.keras.models.load_model("new/prueba.h5")
+    else:
+        modelo = crear_red(img_height, img_width, num_clases, train_ds, val_ds)
     
-
-    # modelo = Sequential([
-    # data_augmentation,
-    # layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
-    # layers.Conv2D(32, 3, padding='same', activation='relu'),
-    # layers.MaxPooling2D(3,3),
-    # layers.Conv2D(64, 3, padding='same', activation='relu'),
-    # layers.MaxPooling2D(3,3),
-    # layers.Dropout(0.2),
-    # layers.Flatten(),
-    # layers.Dense(50, activation='relu'),
-    # layers.Dense(50, activation='relu'),
-    # layers.Dense(num_clases),
-    # layers.Activation("softmax")
-    # ])
-    # modelo.compile(optimizer='adam',
-    #           loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-    #           metrics=['accuracy'])
-    # modelo.summary()
-    # epochs = 200
-    # history = modelo.fit(
-    # train_ds,
-    # validation_data=val_ds,
-    # epochs=epochs)
-    # acc = history.history['accuracy']
-    # val_acc = history.history['val_accuracy']
-
-    # loss = history.history['loss']
-    # val_loss = history.history['val_loss']
-    # modelo.save("prueba.h5")
-
-    modelo = tf.keras.models.load_model("new/prueba.h5")
     test_labels = []
     test_images = [] 
     for img, labels in val_ds.take(1):
@@ -87,7 +100,7 @@ def main():
     test_acc = sum(y_pred == y_true) / len(y_true)
     print(("Test accuracy: {:.2f}%".format(test_acc * 100)))
     consfusion_matrix = tf.math.confusion_matrix(y_true, y_pred)
-    plt.figure(figsize=(10, 10))
+    fig2 = plt.figure(figsize=(12, 7))
     sns.heatmap(consfusion_matrix.numpy(), 
     xticklabels=class_names,
     yticklabels=class_names, 
@@ -96,40 +109,61 @@ def main():
     plt.xlabel('Prediccion')
     plt.ylabel('Real')
     plt.show()
-    sensor(modelo,class_names)
-    # epochs_range = range(epochs)
-    # img             = tf.keras.utils.load_img("foto/img2.jpeg", target_size = (100, 100))
-    # img_array       = tf.keras.utils.img_to_array(img)
-    # img_array       = tf.expand_dims(img_array, 0)
-    # predictions = modelo.predict(img_array)
-    # score = tf.nn.softmax(predictions)
-    # if 100 * np.max(score) > 30.0 and 100* np.max(score) < 31.0:
-    #     print(
-    #     "Esta imagen probablemente pertenece a {} con un {:.2f} porcentaje de seguridad."
-    #     .format(class_names[np.argmax(score)], 100 * np.max(score)))
-    # else:
-    #     print("no se reconoce")
-    # for i in range(5):
-    #     for images, labels in val_ds.take(1):
-    #         predictions = modelo.predict(images)
-    #         score = tf.nn.softmax(predictions[i])
-    #         print(
-    #         "Esta imagen probablemente pertenece a {} con un {:.2f} porcentaje de seguridad."
-    #         .format(class_names[np.argmax(score)], 100 * np.max(score)))
-    #         break
-
-    
-
-
-    #print(val_ds)
-    #y_true = val_ds.map(lambda x: x[1])
-    #precision = sum(y_true == y_pred) / len(y_true)
-    #print(f'Test set accuracy: {precision:.0%}')
+    t_start = threading.Thread(target=sensor, args=(modelo, class_names, ))
+    t_start.start()
         
 
+def crear_red(img_height, img_width, num_clases, train_ds, val_ds):
+    data_augmentation = tf.keras.Sequential(
+    [
+        tf.keras.layers.RandomFlip("horizontal",
+                        input_shape=(img_height,
+                                    img_width,
+                                    3)),
+        tf.keras.layers.RandomRotation(0.1),
+        tf.keras.layers.RandomZoom(0.1),
+    ]
+    )
+    
+    modelo = Sequential([
+        data_augmentation,
+        layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+        layers.Conv2D(32, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(3,3),
+        layers.Conv2D(64, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(3,3),
+        layers.Dropout(0.2),
+        layers.Flatten(),
+        layers.Dense(50, activation='relu'),
+        layers.Dense(50, activation='relu'),
+        layers.Dense(num_clases),
+        layers.Activation("softmax")
+    ])
+    modelo.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+    modelo.summary()
+    epochs = 200
+    history = modelo.fit(
+    train_ds,
+    validation_data=val_ds,
+    epochs=epochs)
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    fig = plt.figure(figsize=(12, 7))
+    plt.plot(history.history['loss'], label='Evolucion')
+    plt.plot(history.history['val_loss'], label = 'val_Evolucion')
+    plt.xlabel('Epoca')
+    plt.ylabel('loss')
+    plt.legend(loc='lower right')
+    modelo.save("new/prueba.h5")
+    return modelo
 
 def sensor(modelo,class_names):
-    arduino = serial.Serial('COM5', 9600)
+    arduino = serial.Serial('COM3', 9600)
     while True:
         rawString = arduino.readline()
         if "True" in str(rawString):
@@ -142,7 +176,10 @@ def sensor(modelo,class_names):
                 img_array       = tf.expand_dims(img_array, 0)
                 predictions = modelo.predict(img_array)
                 score = tf.nn.softmax(predictions)
-                if 100 * np.max(score) > 30.0 and 100* np.max(score) < 31.0:
+                print(
+                    "Esta imagen probablemente pertenece a {} con un {:.2f} porcentaje de seguridad."
+                    .format(class_names[np.argmax(score)], 100 * np.max(score)))
+                if 100 * np.max(score) > 20:
                     print(
                     "Esta imagen probablemente pertenece a {} con un {:.2f} porcentaje de seguridad."
                     .format(class_names[np.argmax(score)], 100 * np.max(score)))
@@ -150,7 +187,6 @@ def sensor(modelo,class_names):
                     print("no se reconoce")
             else:
                 print("no bandera")
-    pass
 
 
 def foto():
@@ -170,4 +206,7 @@ def foto():
     return bandera, nombre
     
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+    ventana = Vista()
+    ventana.show()
+    sys.exit(app.exec_())
